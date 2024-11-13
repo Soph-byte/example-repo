a2f256dc74b07bc40e6e70895b10c394ea3b15f7
gpiren <101645203+gpiren@users.noreply.github.com>
2024-11-13 18:45:47 +0300

    Add files via upload
    Hi. Here's your new dataset. I've decreased the number of variables to 2.

--- script.R
+++ script.R
@@ -4,3 +4,8 @@ data <- starwars |> filter(mass > 60) |> select (name,mass,height)
 
 library(readr)
 write_csv(data,file = "./starwars_modified.csv")
+View(starwars_modified)
+
+data_space_cowboy <- starwars |> filter(mass > 70) |> select(name, mass)
+
+write_csv(data_space_cowboy,file = "./new_data.csv")
